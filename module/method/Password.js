f48e318d2dcf2b226fc4d0967d44e3d6f7d6e343
Artur DevOps <50926041+arturproj@users.noreply.github.com>
2020-06-11 23:24:02 +0200

    update

--- module/method/Password.js
+++ module/method/Password.js
@@ -1,16 +1,34 @@
 const debug = require('./debug.js');
 
+const allCapsAlpha = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; 
+const allLowerAlpha = [..."abcdefghijklmnopqrstuvwxyz"]; 
+const allUniqueChars = [..."~!@#$%^&*()_+-=[]\{}|;:',./<>?"];
+const allNumbers = [..."0123456789"];
+
+const base = [...allCapsAlpha, ...allNumbers, ...allLowerAlpha, ...allUniqueChars];
+const baseString = [...allCapsAlpha,...allLowerAlpha];
+const baseNumber = [...allNumbers];
+const baseASCII = [...allUniqueChars];
+
+const generator = (dbase, len) => {
+   return [...Array(len)]
+     .map(i => dbase[Math.random()*dbase.length|0])
+     .join('');
+};
+
 var PasswordGen = {
     Number : (function (len = 8) {
-        
+        return generator(baseNumber, len)
     }),
     String : (function (len = 8) {
-        
+        return generator(baseString, len)
     }),
     ASCII : (function (len = 8) {
-        
+        return generator(baseASCII, len)
     }),
     Full : (function (len = 8) {
-        
+        return generator(base, len)
     }),
 };
+
+debug.error(PasswordGen.Full(15));
