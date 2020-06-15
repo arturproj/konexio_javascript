304910904de0f4a3e3485122ac83d52f8283c6d3
Artur DevOps <50926041+arturproj@users.noreply.github.com>
2020-06-15 18:34:03 +0200

    DAY 5

--- DAY05/secondGreatLow.js
+++ DAY05/secondGreatLow.js
@@ -1,3 +1,20 @@
+/* Challenge
+ * En JavaScript, créer une fonction secondGreatLow(arr) qui prendra en paramètre un array de nombres stockés dans arr et retournera la deuxième valeur plus petite valeur et deuxième plus grande valeur. Par exemple [7, 7, 12, 98, 106] devrait retourner 12 et 98. L’array ne doit pas être vide and devra contenir au moins deux nombres. Cela peut devenir compliqué s’il n’y a que deux nombres.
+ * 
+ * Challenge 2
+ * Utiliser le process.argv pour créer un programme qui récupère votre input et utilise cette fonction.
+ * 
+ * Echantillon de tests
+ * Input:1, 42, 42, 180
+ * 
+ * Output:“42 42”
+ * 
+ * Input:4, 90
+ * 
+ * Output:“90 4” 
+*/
+
+
 function serialise(value){
     return [...value[0]];
 }
